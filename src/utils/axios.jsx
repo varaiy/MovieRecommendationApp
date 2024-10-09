594b38aa6d7ee6526c395b6f283237d704a615da
varaiy <145434983+varaiy@users.noreply.github.com>
2024-10-09 11:58:51 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
-    headers: {
-        accept: "application/json",
-        Authorization: import.meta.env.VITE_REACT_APP_TOKEN,
-    },
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWM0NjcwZDI5ZDkyNWY4NzdmNzdkMTM4OTRiNTEyNyIsIm5iZiI6MTcyNzU0OTY5MS4zMTk4MTcsInN1YiI6IjY2Zjg0ZTkzMTQwZmJmNmExYTVmNDgyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7sStV5DBJcjbdBui333Y7GI671Rc_Uwy1pBWk30lfnw",
+  },
 });
 
 export default instance;
