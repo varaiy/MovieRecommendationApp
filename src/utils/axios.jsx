09334b9203f385c1bef38814239eee50521223ac
varaiy <145434983+varaiy@users.noreply.github.com>
2024-10-11 00:13:35 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
-    headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_TOKEN}`, 
-        'Access-Control-Allow-Origin': '*',  
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',  
-        'Access-Control-Allow-Headers': 'Authorization, Content-Type',  
-    },
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_TOKEN}`,
+  },
 });
 
 export default instance;
